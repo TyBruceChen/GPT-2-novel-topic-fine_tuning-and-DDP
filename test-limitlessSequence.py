7874f4e47504b53b68f9562a970535d8c1342560
Tianyi (Bruce) Chen <152252677+TyBruceChen@users.noreply.github.com>
2024-09-27 20:27:35 +0800

    Update test-limitlessSequence.py

--- test-limitlessSequence.py
+++ test-limitlessSequence.py
@@ -11,25 +11,23 @@ else:
 output_tokens = torch.tensor(())
 create_output_token = True
 i = 1
-while intend_length >= max_length:
-    gen_tokens = model.generate(
-            input_ids,
-            do_sample=True,
-            temperature=0.9,
-            top_k = 5,
-            max_length=max_length,
-        )
-    
-    if create_output_token == True:
-        #first round
-        output_tokens = gen_tokens
-        create_output_token = False
-        intend_length = intend_length - max_length 
-
-    else:
-
-
-        output_tokens = torch.cat((output_tokens, gen_tokens[0][-i*step_size:].unsqueeze_(dim = 0)), 1)
+        while intend_length >= max_length:
+            gen_tokens = model.generate(
+                    input_ids,
+                    do_sample=True,
+                    temperature=0.9,
+                    top_k = 5,
+                    max_length=max_length,
+                )
+            
+            if create_output_token == True:
+                #first round
+                output_tokens = gen_tokens
+                create_output_token = False
+                intend_length = intend_length - max_length 
+        
+            else:
+                output_tokens = torch.cat((output_tokens, gen_tokens[0][-i*step_size:].unsqueeze_(dim = 0)), 1)
 
     input_ids = gen_tokens[0][step_size:].unsqueeze_(dim = 0)
     intend_length = intend_length - step_size
